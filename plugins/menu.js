affc548eade85748fb7840f2ad174ff309171dd6
Thanujqdilsan <thanujadilsan2011@gmail.com>
2025-08-02 17:12:10 +0530

    Thanuja-king-md

---
+++ plugins/menu.js
@@ -0,0 +1,11 @@
+module.exports = (sock) => {
+  sock.ev.on('messages.upsert', async ({ messages }) => {
+    const msg = messages[0]
+    if (!msg.message) return
+
+    const text = msg.message.conversation || msg.message.extendedTextMessage?.text
+    if (text === '.menu') {
+      await sock.sendMessage(msg.key.remoteJid, { text: 'Hello! I am THANUVA-KING-MD bot.' }, { quoted: msg })
+    }
+  })
+        }
