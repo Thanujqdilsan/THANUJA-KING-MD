eeef937b8b277932acd1e84711cab522d39903ad
Thanujqdilsan <thanujadilsan2011@gmail.com>
2025-08-02 17:07:33 +0530

    Thanuja-king-md

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  owner: ['94702509233'],
+  botName: 'THANUVA-KING-MD',
+  ownerName: 'Thanuja',
+  sessionName: 'session',
+  prefix: ['.', '!'],
+  footer: '© THANUVA-KING-MD',
+  }
